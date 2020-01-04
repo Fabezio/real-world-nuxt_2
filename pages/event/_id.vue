8706ae8968efe40bcb0ad49949b9448f112082e0
fabezio <fabezio@outlook.fr>
2020-01-04 12:45:57 +0100

    muted then/catch into async/await + addd to the id page - progress-bar visible

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,26 +1,33 @@
 <template lang="pug">
 #create
-  h1 Event #
-    | {{id}}
+  h1 {{event.title}}
 </template>
 
 <script>
 export default {
   head() {
     return {
-      title: `Event #${this.id}`,
+      title: this.event.title,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: `All you wanna know about event #${this.id}`
+          content: `All you wanna know about event #${this.event.title}`
         }
       ]
     }
   },
-  computed: {
-    id() {
-      return this.$route.params.id
+  async asyncData({ $axios, error, params }) {
+    try {
+      const { data } = await $axios.get(
+        `http://localhost:3000/events/${params.id}`
+      )
+      return { event: data }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: `Unable to fetch event #${params.id} at this time. Please select another one.`
+      })
     }
   }
 }
