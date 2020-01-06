fc408f8866ec7d2ab3e466c4eff20bf884605948
fabezio <fabezio@outlook.fr>
2020-01-06 10:07:03 +0100

    event elements added (_id)

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,5 +1,22 @@
 <template lang="pug">
-#create
+div
+  .event-header
+    span.eyebrow @{{ event.time }} on {{ parsedDate}}
+    h1.title {{ event.title }}
+    h5 Organized by {{ event.organizer ? event.organizer.name : '' }}
+    h5 Category: {{ event.category }}
+  BaseIcon(name='map')
+    h2 Location
+  address {{ event.location }}
+  h2 Event details
+  p {{ event.description }}
+  h2
+    | Attendees
+    span.badge.-fill-gradient {{ event.attendees.length }}
+  ul.list-group
+    li.list-item(v-for='(attendee, index) in event.attendees', :key='index')
+      b {{ attendee.name }}
+//#create
   h1 {{event.title}}
 </template>
 
@@ -19,7 +36,11 @@ export default {
     }
   },
   computed: mapState({
-    events: (state) => state.events.event
+    event: (state) => state.events.event,
+    parsedDate() {
+      const eventDate = new Date(this.event.date)
+      return eventDate.toDateString()
+    }
   }),
   async fetch({ store, error, params }) {
     try {
